e3560c4355677e491e65268b4bc6bbdd0437314e
Dwayne-Phillips <d.phillips@computer.org>
2020-10-28 17:16:21 -0400

    The first commit of hello1.jl

---
+++ hello1.jl
@@ -0,0 +1,12 @@
+
+# This is a simple example of a file containing a simple function.
+# Inside the Julia REPL, include("hello1.jl")
+# Then use it.
+# 28 October 2020
+
+
+function hello1(a, b)
+   println("This is hello1 ")
+   return(a+b)
+end
+
