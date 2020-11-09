93ea51f2649c46a7906ec55b3a5cc5729f9fa04c
Dwayne-Phillips <d.phillips@computer.org>
2020-11-09 16:35:07 -0500

    Troubles with QuartzImageIO and ImageMagick

--- edge.jl
+++ edge.jl
@@ -8,17 +8,43 @@ over an image to detect edges.
 It uses a simple 3x3 mask that quickly detects
 edges.
 
-You run this in the Julia REPL with include("edge.jl")
+You run this in the Julia REPL with
+include("edge.jl")
+
+You can run this from the command line with
+julia edge.jl
 
 18 October 2020
 
+9 November 2020
+For some unknown reason, the original version that
+used QuartzImageIO stopped working.
+I did the Pkg.add("ImageMagick") instead, and it
+works again.
+I don't understand why???
+
 ==============================================#
+
+# These import and Pkg.add added 9 November 2020
+
+import Pkg
+using Pkg
+Pkg.add("Images")
+Pkg.add("ImageMetadata")
+Pkg.add("TestImages")
+Pkg.add("ImageView")
+Pkg.add("ImageMagick")
+
+
+# Pkg.add("QuartzImageIO")
+
 
 mask = [-1 0 -1; 0 4 0; -1 0 -1];
 
-using Images, ImageMetadata, TestImages, ImageView
-using Pkg
-Pkg.add("QuartzImageIO")
+using Images, ImageMetadata, TestImages, ImageView, FileIO
+
+# using Pkg
+# Pkg.add("QuartzImageIO")
 
 # Read the input image and display it
 input_image = nothing
@@ -27,7 +53,7 @@ imshow(input_image)
 
 # create the output image, initialize it with the input image.
 output_image = nothing
-output_image = load(output_image_path)
+output_image = load("cameraman.tif")
 
 # Here is the convolution loops
 for i in 3:509
@@ -41,12 +67,12 @@ for i in 3:509
       end
       if(sum < 0) sum=0 end
       if(sum > 1.0) sum=1.0 end
-     output_image[i,j] = sum 
+     output_image[i,j] = sum
    end
 end
 
 # Display the output image and save it to disk.
 imshow(output_image)
-save("edgecameraman.tif",output_image)
+save("edgecameramanxx.tif",output_image)
 
 # The End
